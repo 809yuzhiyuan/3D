a200d7f2fb1e44eecda790eed464f1758e57dae2
809yuzhiyuan <19275760046@163.com>
2026-04-19 18:48:37 +0800

    Update main.js

--- main.js
+++ main.js
@@ -219,8 +219,9 @@ function createWorld() {
             const boxGeometry = new THREE.BoxGeometry(boxSize, boxSize, boxSize);
             const boxMaterial = new THREE.MeshBasicMaterial({ color: CONFIG.cBox });
             const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
-            // 放置在房间内的某个位置，比如靠墙
-            boxMesh.position.set(x - CONFIG.w/2 + boxSize/2 + 2, CONFIG.floorH/2, z);
+            // 放置在房间的角落 (-X, -Z)
+            const cornerOffset = CONFIG.w/2 - boxSize/2 - 2; // 距离墙壁2单位
+            boxMesh.position.set(x - cornerOffset, CONFIG.floorH/2, z - cornerOffset); // <--- 修改这里
             scene.add(boxMesh);
             
             // 存储箱子信息
